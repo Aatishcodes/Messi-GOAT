abcc9bbf2063ff1a259b60f511c9666ee8f6b410
Aatishcodes <118838033+Aatishcodes@users.noreply.github.com>
2023-03-01 21:41:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function New_messi_picture() {
+    document.getElementById("profile").src = "https://i.postimg.cc/3wVTf4TB/download.jpg";
+}
+
+function Old_messi_picture() {
+    document.getElementById("profile").src = "https://i.postimg.cc/FKzXGs5Z/download.jpg"
+}
